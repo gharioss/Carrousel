3a7b86e654d8fc2e45d47d4f0df3f049d73b6dfc
Julie Napoli <njulie818@gmail.com>
2022-01-14 18:03:34 +0100

    sarouel

--- script.js
+++ script.js
@@ -2,32 +2,30 @@ var arrayOfImages = ["images/image1.jpg", "images/image2.jpg", "images/image3.jp
 let x = 0
 let y = 0
 document.getElementById('counted').textContent = (x+1) + " / " + arrayOfImages.length
+document.getElementById('counted2').textContent = (y+1) + " / " + arrayOfImages.length
 function switchImage() {
-	document.getElementById('image_to_switch').src= arrayOfImages[y]
 	y += 1
-
-	if (y == 9) {
+	if (y == arrayOfImages.length) {
 		y = 0
 	}
-	console.log(x)
+	document.getElementById('image_to_switch').src= arrayOfImages[y]
+	document.getElementById('counted2').textContent = (y+1) + " / " + arrayOfImages.length
 }
 setInterval(switchImage, 1000);
 
-
 document.getElementById('next').addEventListener('click', function() {
 	x += 1
-	if (x == 9) {
+	if (x == arrayOfImages.length) {
 		x = 0
 	}
 	document.getElementById('image_to_click').src= arrayOfImages[x]
 	document.getElementById('counted').textContent = (x+1) + " / " + arrayOfImages.length
-
 })
 
 document.getElementById('previous').addEventListener('click', function() {
 	x -= 1
 	if (x == -1) {
-		x = 8
+		x = arrayOfImages.length-1
 	}
 	document.getElementById('image_to_click').src= arrayOfImages[x]
 	document.getElementById('counted').textContent = (x+1) + " / " + arrayOfImages.length
